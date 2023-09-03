94d27068c8acf0b80e3e701273de2af863749c09
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-09-03 09:25:58 +0100

    Add project files.

---
+++ GenerateSecretKey.cs
@@ -0,0 +1,23 @@
+﻿namespace GenerateSecretKey
+{
+    using System;
+    using System.Security.Cryptography;
+
+    public static class JwtService
+    {
+        public static string GenerateSecretKey(int length)
+        {
+            const string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            var chars = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(validChars.Length)];
+            }
+
+            return new string(chars);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenerateSecretKey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // You can call the GenerateSecretKey method here
+            string secretKey = GenerateSecretKey(50); // Adjust the length as needed
+            Console.WriteLine($"Generated Secret Key: {secretKey}");
+        }
+
+        static string GenerateSecretKey(int length)
+        {
+            const string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            var chars = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(validChars.Length)];
+            }
+
+            return new string(chars);
+        }
+    }
+}
